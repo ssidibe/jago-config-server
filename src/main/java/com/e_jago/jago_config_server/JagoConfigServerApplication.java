ad17968315dea1e4207945cd188c402557d446f6
Samba SIDIBE <sambasidibe@gmail.com>
2025-12-03 10:14:37 +0000

    Initialisation

---
+++ src/main/java/com/e_jago/jago_config_server/JagoConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.e_jago.jago_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JagoConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JagoConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/e_jago/jago_config_server/JagoConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_jago.jago_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JagoConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
